fbd2a748407316800a9a9901c87952862ca0adaf
vainercesario <vainer.cesario@portergroup.com.br>
2024-12-23 11:08:05 -0300

    feat: adjust dockerfile to build

--- src/PythonCSharp/Program.cs
+++ src/PythonCSharp/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Diagnostics;
 
-internal class Program
+public static class Program
 {
     private static void Main(string[] args)
     {
